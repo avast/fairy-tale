d794cdb916d8a6581af08cb582bb88e3cd154f5f
Janecek Jakub <janecek@avast.com>
2017-12-21 16:51:54 +0100

    Fix travis.sh

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 
 sbt test &&
   if $(test ${TRAVIS_REPO_SLUG} == "avast/scala-fp-toolbox" && test ${TRAVIS_PULL_REQUEST} == "false" && test "$TRAVIS_TAG" != ""); then
-    export VERSION = $TRAVIS_TAG
+    export VERSION=$TRAVIS_TAG
     sbt publish
   else
     exit 0 # skipping publish, it's regular build
