0f2f4888688dc9cf37b042437453649b37bd41cc
Honza Strnad <hanny.strnad@gmail.com>
2018-01-08 14:17:31 +0100

    Monix function k (#5)
    * Moved eval to task functionk to separate package

--- monix/src/main/scala/com/avast/fairytale/functionk/monix/package.scala
+++ monix/src/main/scala/com/avast/fairytale/functionk/monix/package.scala
@@ -1,11 +1,10 @@
-package com.avast.fairytale
+package com.avast.fairytale.functionk
 
 import cats.Eval
 import cats.arrow.FunctionK
-import monix.eval.Task
+import _root_.monix.eval.Task
 
-package object functionk {
-
+package object monix {
   implicit val evalToTask: FunctionK[Eval, Task] = new FunctionK[Eval, Task] {
     override def apply[A](eval: Eval[A]): Task[A] = Task.fromEval(eval)
   }
