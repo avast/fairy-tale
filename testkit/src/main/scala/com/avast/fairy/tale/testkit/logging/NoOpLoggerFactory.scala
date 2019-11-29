45218580fec8b972b3c8b6318dbed63be81637ac
Jan Strnad <strnad@avast.com>
2019-11-29 14:53:08 +0100

    Require applicative only for testkit

--- testkit/src/main/scala/com/avast/fairy/tale/testkit/logging/NoOpLoggerFactory.scala
+++ testkit/src/main/scala/com/avast/fairy/tale/testkit/logging/NoOpLoggerFactory.scala
@@ -1,35 +1,35 @@
 package com.avast.fairy.tale.testkit.logging
 
-import cats.effect.Sync
+import cats.Applicative
 import com.avast.fairytale.logging.{Logger, LoggerFactory, Message}
 import com.avast.utils2.Done
 
 import scala.language.higherKinds
 
-class NoOpLoggerFactory[F[_]: Sync] extends LoggerFactory[F] {
+class NoOpLoggerFactory[F[_]: Applicative] extends LoggerFactory[F] {
   override def make(name: String): Logger[F] = new NoOpLogger
 }
 
-private class NoOpLogger[F[_]: Sync] extends Logger[F] {
+private class NoOpLogger[F[_]: Applicative] extends Logger[F] {
 
-  private val F = Sync[F]
+  private val F = Applicative[F]
 
-  override def error(msg: => Message): F[Done] = F.delay(Done)
+  override def error(msg: => Message): F[Done] = F.pure(Done)
 
-  override def error(ex: Throwable, msg: => Message): F[Done] = F.delay(Done)
+  override def error(ex: Throwable, msg: => Message): F[Done] = F.pure(Done)
 
-  override def warn(msg: => Message): F[Done] = F.delay(Done)
+  override def warn(msg: => Message): F[Done] = F.pure(Done)
 
-  override def warn(ex: Throwable, msg: => Message): F[Done] = F.delay(Done)
+  override def warn(ex: Throwable, msg: => Message): F[Done] = F.pure(Done)
 
-  override def info(msg: => Message): F[Done] = F.delay(Done)
+  override def info(msg: => Message): F[Done] = F.pure(Done)
 
-  override def debug(msg: => Message): F[Done] = F.delay(Done)
+  override def debug(msg: => Message): F[Done] = F.pure(Done)
 
-  override def debug(ex: Throwable, msg: => Message): F[Done] = F.delay(Done)
+  override def debug(ex: Throwable, msg: => Message): F[Done] = F.pure(Done)
 
-  override def trace(msg: => Message): F[Done] = F.delay(Done)
+  override def trace(msg: => Message): F[Done] = F.pure(Done)
 
-  override def trace(ex: Throwable, msg: => Message): F[Done] = F.delay(Done)
+  override def trace(ex: Throwable, msg: => Message): F[Done] = F.pure(Done)
 
 }
