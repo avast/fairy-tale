e628f80c5b03179c4bfe76fbda8fc0a75ddad70b
Honza Strnad <strnad@avast.com>
2019-11-29 14:56:26 +0100

    Require applicative only for testkit (#13)

--- testkit/src/main/scala/com/avast/fairy/tale/testkit/monitoring/NoOpMetricFactory.scala
+++ testkit/src/main/scala/com/avast/fairy/tale/testkit/monitoring/NoOpMetricFactory.scala
@@ -2,13 +2,13 @@ package com.avast.fairy.tale.testkit.monitoring
 
 import java.time.Duration
 
-import cats.effect.Sync
+import cats.Applicative
 import com.avast.fairytale.monitoring._
 import com.avast.utils2.Done
 
 import scala.language.higherKinds
 
-class NoOpMetricFactory[F[_]: Sync] extends MetricFactory[F] {
+class NoOpMetricFactory[F[_]: Applicative] extends MetricFactory[F] {
 
   override def named(name: String): MetricFactory[F] = this
 
@@ -25,56 +25,56 @@ class NoOpMetricFactory[F[_]: Sync] extends MetricFactory[F] {
   override def gauge: GaugeFactory[F] = new NoOpGaugeFactory
 }
 
-private class NoOpMeter[F[_]: Sync] extends Meter[F] {
+private class NoOpMeter[F[_]: Applicative] extends Meter[F] {
 
-  private val F = Sync[F]
+  private val F = Applicative[F]
 
-  override def mark: F[Done] = F.delay(Done)
+  override def mark: F[Done] = F.pure(Done)
 
-  override def mark(n: Long): F[Done] = F.delay(Done)
-
-}
-
-private class NoOpCounter[F[_]: Sync] extends Counter[F] {
-
-  private val F = Sync[F]
-
-  override def inc: F[Done] = F.delay(Done)
-
-  override def inc(n: Long): F[Done] = F.delay(Done)
-
-  override def dec: F[Done] = F.delay(Done)
-
-  override def dec(n: Int): F[Done] = F.delay(Done)
+  override def mark(n: Long): F[Done] = F.pure(Done)
 
 }
 
-private class NoOpTimer[F[_]: Sync] extends Timer[F] {
+private class NoOpCounter[F[_]: Applicative] extends Counter[F] {
 
-  private val F = Sync[F]
+  private val F = Applicative[F]
+
+  override def inc: F[Done] = F.pure(Done)
+
+  override def inc(n: Long): F[Done] = F.pure(Done)
+
+  override def dec: F[Done] = F.pure(Done)
+
+  override def dec(n: Int): F[Done] = F.pure(Done)
+
+}
+
+private class NoOpTimer[F[_]: Applicative] extends Timer[F] {
+
+  private val F = Applicative[F]
 
   type Context = Long
 
-  override def start: F[Context] = F.delay(System.nanoTime())
+  override def start: F[Context] = F.pure(System.nanoTime())
 
-  override def stop(context: Context): F[Duration] = F.delay(Duration.ofNanos(System.nanoTime() - context))
-
-}
-
-private class NoOpHistogram[F[_]: Sync] extends Histogram[F] {
-
-  private val F = Sync[F]
-
-  override def update(value: Long): F[Done] = F.delay(Done)
+  override def stop(context: Context): F[Duration] = F.pure(Duration.ofNanos(System.nanoTime() - context))
 
 }
 
-private class NoOpGaugeFactory[F[_]: Sync] extends GaugeFactory[F] {
+private class NoOpHistogram[F[_]: Applicative] extends Histogram[F] {
+
+  private val F = Applicative[F]
+
+  override def update(value: Long): F[Done] = F.pure(Done)
+
+}
+
+private class NoOpGaugeFactory[F[_]: Applicative] extends GaugeFactory[F] {
   override def long(name: String): Gauge[F, Long] = new NoOpGauge
 
   override def double(name: String): Gauge[F, Double] = new NoOpGauge
 }
 
-private class NoOpGauge[F[_]: Sync, T] extends Gauge[F, T] {
-  override def set(value: T): F[Done] = Sync[F].delay(Done)
+private class NoOpGauge[F[_]: Applicative, T] extends Gauge[F, T] {
+  override def set(value: T): F[Done] = Applicative[F].pure(Done)
 }
