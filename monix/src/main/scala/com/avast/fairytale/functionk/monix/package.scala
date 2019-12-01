ee4f9322af97f9417f6a31f942ffd90c84100e64
Honza Strnad <strnad@avast.com>
2019-12-01 16:00:31 +0100

    Updated dependencies (#14)

--- monix/src/main/scala/com/avast/fairytale/functionk/monix/package.scala
+++ monix/src/main/scala/com/avast/fairytale/functionk/monix/package.scala
@@ -6,7 +6,7 @@ import _root_.monix.eval.Task
 
 package object monix {
   implicit val evalToTask: FunctionK[Eval, Task] = new FunctionK[Eval, Task] {
-    override def apply[A](eval: Eval[A]): Task[A] = Task.fromEval(eval)
+    override def apply[A](eval: Eval[A]): Task[A] = Task.from(eval)
   }
 
 }
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,19 +1,19 @@
 import sbt._
 
 object Dependencies {
-  lazy val metricsLibrary = "com.avast.metrics" %% "metrics-scala" % "2.5.3"
-  lazy val metricsJmx = "com.avast.metrics" % "metrics-jmx" % "2.5.3"
+  lazy val metricsLibrary = "com.avast.metrics" %% "metrics-scala" % "2.6.6"
+  lazy val metricsJmx = "com.avast.metrics" % "metrics-jmx" % "2.6.6"
 
   lazy val utilsDone = "com.avast.utils2" %% "utils-done" % "10.0.0"
 
-  lazy val cats = "org.typelevel" %% "cats-core" % "1.5.0"
-  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "0.10.1"
+  lazy val cats = "org.typelevel" %% "cats-core" % "2.0.0"
+  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "2.0.0"
 
-  lazy val monixLibrary = "io.monix" %% "monix" % "3.0.0-RC1"
+  lazy val monixLibrary = "io.monix" %% "monix" % "3.1.0"
 
   lazy val slf4jLibrary = "org.slf4j" % "slf4j-api" % "1.7.26"
 
-  lazy val simulacrum = "com.github.mpilquist" %% "simulacrum" % "0.15.0"
+  lazy val simulacrum = "org.typelevel" %% "simulacrum" % "1.0.0"
   lazy val mainecoonCore = "com.kailuowang" %% "mainecoon-core" % "0.6.4"
   lazy val mainecoonMacros = "com.kailuowang" %% "mainecoon-macros" % "0.6.4"
 
