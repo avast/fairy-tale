7931cf73c5fe848237d841599e10fcd4f303ebb6
Tomáš Heřman <hermant@avast.com>
2020-02-26 09:15:53 +0100

    cross publish

--- .travis.sh
+++ .travis.sh
@@ -3,7 +3,7 @@
 sbt test &&
   if $(test ${TRAVIS_REPO_SLUG} == "avast/fairy-tale" && test ${TRAVIS_PULL_REQUEST} == "false" && test "$TRAVIS_TAG" != ""); then
     export VERSION=$TRAVIS_TAG
-    sbt publish
+    sbt +publish
   else
     exit 0 # skipping publish, it's regular build
   fi
